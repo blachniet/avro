bf610c57ee07f8e90831f1a193f0f08288f58869
Brian Lachniet <blachniet@gmail.com>
2019-09-17 21:17:38 -0400

    AVRO-2550: Add env var for Yetus release in Travis

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -24,12 +24,11 @@ case "$TRAVIS_OS_NAME" in
     # sudo apt-get -q clean
     # sudo rm -rf /var/lib/apt/lists/*
 
-    # Only Yetus 0.9.0+ supports `ADD` and `COPY` commands in Dockerfile
-    curl -L https://www-us.apache.org/dist/yetus/0.10.0/apache-yetus-0.10.0-bin.tar.gz | tar xvz -C /tmp/
+    curl -L "https://www-us.apache.org/dist/yetus/${YETUS_RELEASE}/apache-yetus-${YETUS_RELEASE}-bin.tar.gz" | tar xvz -C /tmp/
     # A dirty workaround to disable the Yetus robot for TravisCI,
     # since it'll cancel the changes that .travis/script.sh will do,
     # even if the `--dirty-workspace` option is specified.
-    rm /tmp/apache-yetus-0.10.0/lib/precommit/robots.d/travisci.sh
+    rm "/tmp/apache-yetus-${YETUS_RELEASE}/lib/precommit/robots.d/travisci.sh"
     ;;
 "windows")
     choco install dotnetcore-sdk --version 2.2.300
