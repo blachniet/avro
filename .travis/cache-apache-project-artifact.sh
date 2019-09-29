572bb807993f80f805375a22fd617c29afda3033
Brian Lachniet <blachniet@gmail.com>
2019-09-29 13:31:43 -0400

    Use HTTPS URLs

--- .travis/cache-apache-project-artifact.sh
+++ .travis/cache-apache-project-artifact.sh
@@ -7,7 +7,7 @@
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
 #
-#   http://www.apache.org/licenses/LICENSE-2.0
+#   https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
@@ -15,6 +15,9 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+
+# This script was copied from the HBase project:
+# https://github.com/apache/hbase/blob/1170f28122d9d36e511ba504a5263ec62e11ef6a/dev-support/jenkins-scripts/cache-apache-project-artifact.sh#L1
 
 set -e
 function usage {
